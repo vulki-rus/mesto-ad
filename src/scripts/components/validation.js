2e07858494e3db1de19df43d058f9c2d1c62f93b
vulki-rus <playvulkanator@gmail.com>
2026-03-14 23:47:54 +0400

    final commit

---
+++ src/scripts/components/validation.js
@@ -0,0 +1,112 @@
+const showInputError = (formElement, inputElement, errorMessage, settings) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  if (!errorElement) return;
+
+  inputElement.classList.add(settings.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(settings.errorClass);
+};
+
+const hideInputError = (formElement, inputElement, settings) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  if (!errorElement) return;
+
+  inputElement.classList.remove(settings.inputErrorClass);
+  errorElement.textContent = "";
+  errorElement.classList.remove(settings.errorClass);
+};
+
+const checkInputValidity = (formElement, inputElement, settings) => {
+  if (inputElement.validity.patternMismatch || inputElement.validity.typeMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      settings,
+    );
+    return;
+  }
+
+  const trimmedValueLength = inputElement.value.trim().length;
+  const minLength = parseInt(inputElement.getAttribute("minlength"));
+  if (trimmedValueLength < minLength) {
+    inputElement.setCustomValidity(`Пожалуйста, используйте не менее ${minLength} символов.`);
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      settings,
+    );
+    return;
+  }
+
+  inputElement.setCustomValidity("");
+  hideInputError(formElement, inputElement, settings);
+};
+
+const hasInvalidInput = (inputsElements) => {
+  return inputsElements.some((inputElement) => !inputElement.validity.valid);
+};
+
+const disableSubmitButton = (buttonElement, settings) => {
+  buttonElement.disabled = true;
+  buttonElement.classList.add(settings.inactiveButtonClass);
+};
+
+const enableSubmitButton = (buttonElement, settings) => {
+  buttonElement.disabled = false;
+  buttonElement.classList.remove(settings.inactiveButtonClass);
+};
+
+const toggleButtonState = (inputsElements, buttonElement, settings) => {
+  if (hasInvalidInput(inputsElements)) {
+    disableSubmitButton(buttonElement, settings);
+  } else {
+    enableSubmitButton(buttonElement, settings);
+  }
+};
+
+const setEventListeners = (formElement, settings) => {
+  const inputsElements = Array.from(
+    formElement.querySelectorAll(settings.inputSelector),
+  );
+  const buttonElement = formElement.querySelector(
+    settings.submitButtonSelector,
+  );
+
+  toggleButtonState(inputsElements, buttonElement, settings);
+
+  inputsElements.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement, settings);
+      toggleButtonState(inputsElements, buttonElement, settings);
+    });
+  });
+};
+
+export const clearValidation = (formElement, settings) => {
+  const inputsElements = Array.from(
+    formElement.querySelectorAll(settings.inputSelector),
+  );
+  const buttonElement = formElement.querySelector(
+    settings.submitButtonSelector,
+  );
+
+  inputsElements.forEach((inputElement) => {
+    hideInputError(formElement, inputElement, settings);
+    inputElement.setCustomValidity("");
+  });
+
+  disableSubmitButton(buttonElement, settings);
+};
+
+export const enableValidation = (settings) => {
+  const formsElements = Array.from(
+    document.querySelectorAll(settings.formSelector),
+  );
+
+  formsElements.forEach((formElement) => {
+    setEventListeners(formElement, settings);
+  });
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,6 +9,21 @@
 import { initialCards } from "./cards.js";
 import { createCardElement, deleteCard, likeCard } from "./components/card.js";
 import { openModalWindow, closeModalWindow, setCloseModalWindowEventListeners } from "./components/modal.js";
+import { enableValidation, clearValidation } from './components/validation.js';
+
+// Создание объекта с настройками валидации
+const validationSettings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+enableValidation(validationSettings);
 
 // DOM узлы
 const placesWrap = document.querySelector(".places__list");
@@ -84,16 +99,22 @@ avatarForm.addEventListener("submit", handleAvatarFromSubmit);
 openProfileFormButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
+  // Добавил очистку валидации
+  clearValidation(profileForm, validationSettings);
   openModalWindow(profileFormModalWindow);
 });
 
 profileAvatar.addEventListener("click", () => {
   avatarForm.reset();
+  // Добавил очистку валидации
+  clearValidation(avatarForm, validationSettings);
   openModalWindow(avatarFormModalWindow);
 });
 
 openCardFormButton.addEventListener("click", () => {
   cardForm.reset();
+  // Добавил очистку валидации
+  clearValidation(cardForm, validationSettings);
   openModalWindow(cardFormModalWindow);
 });
 
